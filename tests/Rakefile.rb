9865342a2555f55b472dc87ca3ad7461ccd380ab
sunjeet-khokhar <sunjeet81@gmail.com>
2015-11-07 09:27:46 +1300

    Wrote a rake file

---
+++ tests/Rakefile.rb
@@ -0,0 +1,15 @@
+#on the CLI use the command "rake spec" ...to invoke this rake file , which will in turn invoke your tests in the spec directory with the options mentioned in the rakefile
+#
+
+
+require 'rake'
+require 'rspec/core/rake_task'
+ 
+RSpec::Core::RakeTask.new(:spec) do |t|
+t.pattern = Dir.glob('spec/*_spec.rb')
+t.rspec_opts = '--format documentation --out rspec.txt'
+# t.rspec_opts << ' more options'
+#t.rcov = true
+
+end
+task :default => :spec
