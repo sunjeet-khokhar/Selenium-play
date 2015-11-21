cf812003ef456aa57d2029c88f0fb2348f47effd
sunjeet-khokhar <sunjeet81@gmail.com>
2015-11-21 18:34:24 +1300

    Dropdown test
    Created a ruby test to select and verify text from a drop down

---
+++ ruby_tests/dropdown.rb
@@ -0,0 +1,29 @@
+require 'selenium-webdriver'
+require 'rspec/expectations'
+include RSpec::Matchers
+
+
+  @driver = Selenium::WebDriver.for :firefox
+  
+  @driver.get 'http://the-internet.herokuapp.com/dropdown'
+
+  #find eement called dropdown on the web page
+  dropdown = @driver.find_element(id: 'dropdown')
+  #use Webdriver API's Select class to create a new selection object and assign it a variable
+  select_list = Selenium::WebDriver::Support::Select.new(dropdown)
+  #select the text 'option1'
+  select_list.select_by(:text, 'Option 1')
+  #you can select by value as well
+  # select_list.select_by(:value, '1')
+  #what is the selected text ?
+  selected_option = select_list.selected_options[0].text
+  #some RSPEC crap i dont understand !
+  expect(selected_option).to eql 'Option 1gvgvvg'
+  #prints what was selected ?
+  puts selected_option
+  #remeber to kill the browser ! 
+  @driver.quit
+  
+ 
+
+  
